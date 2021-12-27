316639d0b385eed9fe28512abab1fec29aa4338a
Hanan Somily <hanan.somily.jiz.s@tuwaiq.edu.sa>
2021-12-27 08:00:33 +0300

    user interface and view controller

---
+++ CarFixApp/CarFixApp/Controller/LoginUserViewController.swift
@@ -0,0 +1,29 @@
+//
+//  loginUserViewController.swift
+//  CarFixApp
+//
+//  Created by Hanan Somily on 27/12/2021.
+//
+
+import UIKit
+
+class LoginUserViewController: UIViewController {
+
+    @IBOutlet weak var emailUserTextField: UITextField!
+    
+    @IBOutlet weak var passwardUserTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+    @IBAction func handelLogin(_ sender: Any) {
+        
+    }
+    
+    @IBAction func handelRegister(_ sender: Any) {
+    }
+}
---
+++ CarFixApp/CarFixApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarFixApp
+//
+//  Created by Hanan Somily on 26/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
